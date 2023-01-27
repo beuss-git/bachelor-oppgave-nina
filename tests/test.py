efae464baa6d057bc8b4187ffa40bfcf0ff376ca
Beuss <beuss.code@gmail.com>
2023-01-27 16:23:40 +0100

    Add initial setup files
    Includes:
    * requirements.txt
    * sample test
    * settings.json for vscode
    * gitignore

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+"""This is a test module for python"""
+
+
+def test():
+    """this is a test function for python
+
+    Returns:
+        boolean: true
+    """
+    msg = "Hello, python!"
+
+    print(msg)
+    return True
