74ba5eebaa766a50ea146c9c51606bf391b757e8
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2023-01-31 00:23:33 +0100

    [#8] Add first browse files area and some default window settings

--- app/myApp.py
+++ app/myApp.py
@@ -5,43 +5,92 @@ import typing
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QLabel,
-    QLineEdit,
     QVBoxLayout,
-    QWidget,
     QFileDialog,
+    QPushButton,
+    QLabel,
+    QWidget,
+    QLineEdit,
+    QHBoxLayout,
 )
+from PyQt6 import QtGui
+from PyQt6.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
     """Main Window"""
 
-    def __init__(self) -> None:
+    OpenFile = 0
+    OpenFiles = 1
+    OpenDirectory = 2
+    SaveFile = 3
+
+    def __init__(self, mode=OpenFile) -> None:
+        """Initializer
+
+        Args:self
+            mode (int, optional): [description]. Defaults to OpenFile."""
         super().__init__()
 
-        self.setWindowTitle("My App")
-        self.resize(700, 400)
+        # Set default window settings
+        self.window_width, self.window_height = 700, 400
+        self.setWindowTitle("Fish detector 3000")
+        self.setMinimumSize(self.window_width, self.window_height)
+        self.setWindowIcon(QtGui.QIcon("app/images/app_logo.png"))
 
-        self.label = QLabel()
+        wid = QWidget(self)
+        # self.setCentralWidget(wid)
+        v_layout = QVBoxLayout()
+        wid.setFixedWidth(self.window_width)
+        wid.setFixedHeight(int(self.window_height // 2))
+        wid.setLayout(v_layout)
 
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
+        wid2 = QWidget(self)
+        # self.setCentralWidget(wid2)
+        # wid2.move(0, int(self.window_height // 1.5))
+        wid2.setFixedWidth(self.window_width)
+        wid2.setFixedHeight(int(self.window_height // 2) + 50)
+        h_layout = QHBoxLayout()
+        wid2.setLayout(h_layout)
 
-        self.browse_files()
+        self.browser_mode = mode
+        self.filter_name = "All files (*)"
+        # self.dirpath = QDir.currentPath()
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
+        # Text label
+        self.label = QLabel("Folder to analyze:")
+        self.label.setFixedWidth(120)
+        self.label.setFont(
+            QtGui.QFont("Arial", weight=QtGui.QFont.Weight.Bold, pointSize=10)
+        )
+        self.label.setAlignment(
+            Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter
+        )
+        self.label.adjustSize()
+        v_layout.addWidget(self.label)
 
-        container = QWidget()
-        container.setLayout(layout)
+        # Text input
+        self.lineEdit = QLineEdit(self)
+        self.lineEdit.setFixedWidth(int(self.window_width / 2))
+        h_layout.addWidget(self.lineEdit)
+
+        # Browse button
+        browseBtn = QPushButton("Browse Files")
+        browseBtn.setFixedWidth(100)
+        browseBtn.clicked.connect(self.browse_files)
+        h_layout.addWidget(browseBtn)
+        h_layout.addStretch()
 
         # Set the central widget of the Window.
-        self.setCentralWidget(container)
+        # self.show()
+        # self.setCentralWidget(button)
 
-    def browse_files(self) -> typing.Tuple[str, str]:
+    def browse_files(self) -> typing.Tuple[typing.List[str], str]:
         """Open a dialog to select a file, and return the path to it."""
-        filename = QFileDialog.getOpenFileName()
+        filename = QFileDialog.getOpenFileNames()
+        print(filename)
+        self.lineEdit.setText(filename[0][0])
+
         return filename
 
 
