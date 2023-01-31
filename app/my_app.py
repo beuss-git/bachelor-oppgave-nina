f7b085c5bfaf78c9a5904f18beae621203a31481
Beuss <beuss.code@gmail.com>
2023-01-31 15:53:42 +0100

    Add stricter linting

---
+++ app/my_app.py
@@ -0,0 +1,56 @@
+"""Module for testing PyQt6"""
+import sys
+import typing
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+    QFileDialog,
+)
+
+class MainWindow(QMainWindow):
+    """Main Window"""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        self.resize(700, 400)
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        self.browse_files()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
+
+    def browse_files(self) -> typing.Tuple[str, str]:
+        """Open a dialog to select a file, and return the path to it."""
+        filename = QFileDialog.getOpenFileName()
+        return filename
+
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
