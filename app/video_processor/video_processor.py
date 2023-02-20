010aef8e639ac1af752902b474dae38f09b42864
Beuss <beuss.code@gmail.com>
2023-02-20 18:32:30 +0100

    fix: add support for videos with a lot of predictions
    Previously when encountering videos with a lot of prediction results,
    the resulting "-filter_complex" argument of ffmpeg would be too long.
    
    The solution was to use "-filter_complex_script" and pass the filter stream through
    a text file instead of as a "raw string" in the command.

--- app/video_processor/video_processor.py
+++ app/video_processor/video_processor.py
@@ -1,6 +1,28 @@
 """Video processor module. Contains functions for processing videos."""
 from typing import List, Tuple
+import os
+import subprocess
 import ffmpeg
+
+
+def __run_ffmpeg(
+    args: List[str],
+    pipe_stdin: bool = False,
+    pipe_stdout: bool = False,
+    pipe_stderr: bool = False,
+    quiet: bool = False,
+) -> Tuple[bytes, bytes]:
+    stdin_stream = subprocess.PIPE if pipe_stdin else None
+    stdout_stream = subprocess.PIPE if pipe_stdout or quiet else None
+    stderr_stream = subprocess.PIPE if pipe_stderr or quiet else None
+    with subprocess.Popen(
+        args, stdin=stdin_stream, stdout=stdout_stream, stderr=stderr_stream
+    ) as process:
+        out, err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            raise ffmpeg.Error("ffmpeg", out, err)
+    return out, err
 
 
 def cut_video(
@@ -15,7 +37,8 @@ def cut_video(
     """
 
     input_file = ffmpeg.input(input_path)
-    (
+    # Save the concat filter to a file and run filter_complex_script on that
+    command = (
         ffmpeg.concat(
             *[
                 input_file.trim(start_frame=start_frame, end_frame=end_frame)
@@ -27,5 +50,28 @@ def cut_video(
         )
         .output(output_path)
         .overwrite_output()
-        .run()
     )
+
+    args = command.compile()
+
+    # NOTE: The filter_complex argument is too long to be passed as a command line argument.
+    # ffmpeg-python doesn't support filter_complex_script so we need to manually invoke it.
+
+    # find the index of the -filter_complex argument
+    filter_complex_index = args.index("-filter_complex")
+
+    # replace the -filter_complex argument with -filter_complex_script
+    args[filter_complex_index] = "-filter_complex_script"
+
+    # write the filter_complex string to a file
+    with open("filter_complex.txt", "w", encoding="utf-8") as file:
+        file.write(args[filter_complex_index + 1])
+
+    # replace the filter_complex string with the path to the file
+    args[filter_complex_index + 1] = "filter_complex.txt"
+
+    # run ffmpeg with the arguments
+    __run_ffmpeg(args)
+
+    # Delete the filter_complex file
+    os.remove("filter_complex.txt")
