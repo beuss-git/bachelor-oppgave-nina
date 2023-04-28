469c25facff4b206fee48111c9a0549c964dd700
Beuss <beuss.code@gmail.com>
2023-04-28 21:00:43 +0200

    feat: make user confirm that they want to delete the original video

--- app/widgets/widgets_panels.py
+++ app/widgets/widgets_panels.py
@@ -1,11 +1,27 @@
 """Widgets Panels for the application."""
 from dataclasses import dataclass
 
-from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout
+from PyQt6.QtWidgets import QHBoxLayout, QMessageBox, QVBoxLayout
 
+from app import settings
 from app.common import Common
 from app.widgets.file_browser import FileBrowser
 from app.widgets.widgets_options import Checkbox, DropDownWidget
+
+
+def confirmation_dialog(message: str, title: str) -> int:
+    """Shows a confirmation dialog when checkbox"""
+
+    message_box = QMessageBox()
+    message_box.setIcon(QMessageBox.Icon.Warning)
+    message_box.setText(message)
+    message_box.setWindowTitle(title)
+    message_box.setStandardButtons(
+        QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+    )
+    message_box.setDefaultButton(QMessageBox.StandardButton.No)
+
+    return message_box.exec()
 
 
 @dataclass
@@ -48,6 +64,27 @@ class WidgetsPanel:
         )
 
         parent_layout.addLayout(buffer_layout)
-        parent_layout.addWidget(
-            Checkbox("Keep original video", "Whether to keep the original video or not")
+        keep_original_cb = Checkbox(
+            "Keep original video", "Whether to keep the original video or not"
         )
+        keep_original_cb.set_check_state(settings.keep_original)
+
+        def on_keep_original_cb_toggled(state: bool) -> None:
+            if not state:
+                # Set checkbox to checked so it isn't unchecked while the dialog is open
+                if (
+                    confirmation_dialog(
+                        "Are you sure you don't want to keep the original video?",
+                        "Delete original video",
+                    )
+                    == QMessageBox.StandardButton.Yes
+                ):
+                    settings.keep_original = state
+                else:
+                    # Keep the checkbox checked
+                    keep_original_cb.set_check_state(True)
+            settings.keep_original = state
+
+        keep_original_cb.connect(on_keep_original_cb_toggled)
+
+        parent_layout.addWidget(keep_original_cb)
