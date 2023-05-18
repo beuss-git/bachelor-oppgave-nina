dc1ea490a99066501f67c3889f1c1c9cbdfea6bf
Beuss <beuss.code@gmail.com>
2023-05-18 16:31:28 +0200

    feat: update training script and config
    This now reflects what we actually used during training :^)

--- model_research/train.py
+++ model_research/train.py
@@ -5,19 +5,44 @@ from ultralytics import YOLO
 # Use if you want to debug
 # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
-if __name__ == "__main__":
-    ultralytics.checks()
-    model = YOLO("yolov8s.pt")
-    model.train(
-        data="nina_new.yaml",
-        epochs=100,
-        val=True,
-        batch=16,
-        imgsz=640,
-        device=0,
-        half=True,
-        workers=8,
-        v5loader=True,
-    )  # train the model
 
-    # model.val(data="brackish.yaml", batch_size=8)  # test the model
+def train(resume: bool) -> None:
+    """Train the model.
+
+    Args:
+        resume: If True, resume training from last checkpoint.
+    """
+    # Initialize with starting weights
+    model = YOLO("yolov8s.pt")
+
+    # Train
+    model.train(
+        resume=resume,
+        data="nina.yaml",
+        epochs=120,
+        val=True,
+        batch=8,
+        imgsz=640,
+        device=None,
+        close_mosaic=10,
+        lr0=0.001,
+        lrf=0.01,
+        weight_decay=0.001,
+        degrees=10,
+        scale=0.6,
+        perspective=0.0005,
+        flipud=0.01,
+        hsv_h=0.2,
+        hsv_s=0.7,
+        hsv_v=0.7,
+        v5loader=True,
+        workers=16,
+    )
+
+
+if __name__ == "__main__":
+    # Perform checks
+    ultralytics.checks()
+
+    # Start/resume training
+    train(resume=False)
